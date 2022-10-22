3c7f7f29b4955439a3ff57d6687b55a2a04f9245
Apollo-mission <115931558+Apollo-mission@users.noreply.github.com>
2022-10-22 13:30:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ const daysSince = Math.floor(difference / millisecondsPerDay);
 
 console.log(difference);
 document.getElementById('elapsed-time').textContent = daysSince;
+
+
+
+const countDownDate = new Date(2023, 0, 26).getTime();
+
+const countDown = setInterval(function() {
+
+    const now = new Date().getTime();
+  
+    const distance = countDownDate - now;
+
+    console.log(now);
+  
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.querySelector(".craquages-number").textContent = "J-" + days + ":" + hours + ":"
+    + minutes + ":" + seconds;
+  
+    if (distance < 0) {
+      clearInterval(countDown);
+      document.querySelector(".craquages-number").textContent = "challenge accompli !";
+    }
+  }, 1000);
