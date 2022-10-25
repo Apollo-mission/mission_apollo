7a6e05eb6a48c6ce0d870181cfa245fac9d9dd5a
Apollo-mission <115931558+Apollo-mission@users.noreply.github.com>
2022-10-25 10:36:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const initialDate = new Date(2022, 9, 17); 
-const now = Date.now();
-const difference = now - initialDate;
-const millisecondsPerDay = 24 * 60 * 60 * 1000;
-const daysSince = Math.floor(difference / millisecondsPerDay);
+function countDays() {
+  const initialDate = new Date(2022, 9, 17); 
+  const now = Date.now();
+  const difference = now - initialDate;
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+  const daysSince = Math.floor(difference / millisecondsPerDay);
 
-console.log(difference);
-document.getElementById('elapsed-time').textContent = daysSince;
+  const selectTime = document.getElementById('elapsed-time');
+  if (selectTime === null) {
+    clearInterval(countDown);
+    return;
+  }
+  else {
+    console.log(selectTime);
+    selectTime.textContent = daysSince;
+  }
+  
+}
 
 
 
@@ -32,3 +42,24 @@ const countDown = setInterval(function() {
       document.querySelector(".craquages-number").textContent = "challenge accompli !";
     }
   }, 1000);
+
+  function hideRocket() {
+    const selectMainContainer = document.querySelector('.stats-container');
+    const selectRocket = document.querySelector('.take-off-rocket');
+    console.log(selectRocket);
+    if(selectRocket === null) {
+      return;
+    }
+    else {
+      
+      setTimeout(() => selectRocket.style.display='none', 4000);
+      setTimeout(() => selectMainContainer.style.overflowY = 'scroll', 4000)
+    }
+  }
+
+  function init() {
+    countDays();
+    hideRocket();
+  }
+
+  init();
